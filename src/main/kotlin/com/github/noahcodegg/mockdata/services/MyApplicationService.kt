5e1324bad1ca2620276737c107b7ffcbe2a36f34
GitHub Action <action@github.com>
2022-01-20 13:05:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/noahcodegg/mockdata/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/noahcodegg/mockdata/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.noahcodegg.mockdata.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.noahcodegg.mockdata.MyBundle
 
 class MyApplicationService {
 
